abd9ad57170bb7a99d1a0d5d4ff3b492dc0dff75
Adriana R.T <adrianapeque78@gmail.com>
2026-08-06 11:27:36 -0600

    Prueba componente alumnos

--- fronted/src/pages/Alumnos.jsx
+++ fronted/src/pages/Alumnos.jsx
@@ -1,5 +1,36 @@
+import { Button } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+
 function Alumnos() {
-  return <h1>Alumnos</h1>;
+  return (
+    <div style={{ padding: "30px" }}>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          marginBottom: "30px"
+        }}
+      >
+        <div>
+          <h1>Alumnos</h1>
+          <p>Alumnos inscritos oficialmente en el ciclo escolar.</p>
+        </div>
+
+        <Button
+          variant="contained"
+          startIcon={<AddIcon />}
+        >
+          Nuevo alumno
+        </Button>
+
+      </div>
+
+      <p>Aquí aparecerá la lista de alumnos.</p>
+
+    </div>
+  );
 }
 
 export default Alumnos;
