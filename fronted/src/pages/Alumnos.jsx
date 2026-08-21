2e7a4345232f743a3c92411a39618c83ba225641
Adriana R.T <adrianapeque78@gmail.com>
2026-08-20 18:09:32 -0600

    Agregar búsqueda por folio en alumnos

--- fronted/src/pages/Alumnos.jsx
+++ fronted/src/pages/Alumnos.jsx
@@ -5,6 +5,7 @@ import VisibilityIcon from "@mui/icons-material/Visibility";
 function Alumnos() {
 
   const [alumnos, setAlumnos] = useState([]);
+  const [busquedaFolio, setBusquedaFolio] = useState("");
   const navigate = useNavigate();
   useEffect(() => {
     fetch("https://sistemainscripcionesfridakahlo.onrender.com/inscripciones/alumnos")
@@ -19,7 +20,14 @@ function Alumnos() {
       <h1>Alumnos</h1>
 
       <p>Alumnos inscritos oficialmente.</p>
-
+      <div style={{ marginTop: "20px", marginBottom: "20px" }}>
+        <input
+          type="text"
+          placeholder="Buscar por folio"
+          value={busquedaFolio}
+          onChange={(e) => setBusquedaFolio(e.target.value)}
+        />
+      </div>
       <table
         style={{
           width: "100%",
@@ -38,7 +46,9 @@ function Alumnos() {
 
         <tbody>
 
-          {alumnos.map((alumno) => (
+          {alumnos.filter((alumno) =>
+            alumno.folio?.toString().toLowerCase().includes(busquedaFolio.toLowerCase())
+          ).map((alumno) => (
 
             <tr key={alumno.id}>
               <td>{alumno.folio}</td>
